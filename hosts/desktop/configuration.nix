e8f39172ffea9515c44d09f834029ab156330a96
NotFlitze <167577764+NotFlitze@users.noreply.github.com>
2025-06-01 11:18:53 +0200

    changed configuration.nix for laptop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -13,7 +13,7 @@
 
       ../../modules/nixosModules/defaults/gui_computer_defaults.nix
 
-      ../../modules/nixosModules/packages/drivers/_drivers-default.nix                     # extra import, change path!!
+      ../../modules/nixosModules/packages/drivers/_drivers-default.nix
     ];
 
   modules.trackpad.enable = false;
--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -10,65 +10,11 @@
   imports = [
       ./hardware-configuration.nix
 
-      ../../modules/nixosModules/steam.nix
-      ../../modules/nixosModules/spotify.nix
-      ../../modules/nixosModules/locates.nix
-      ../../modules/nixosModules/bootloader.nix
-      ../../modules/nixosModules/enableflakes.nix
-      ../../modules/nixosModules/allowunfree.nix
-      ../../modules/nixosModules/tree.nix
-      ../../modules/nixosModules/cmatrix.nix
-      ../../modules/nixosModules/grim.nix
-      ../../modules/nixosModules/gnome-tweaks.nix
-      ../../modules/nixosModules/wl-clipboard.nix
-      ../../modules/nixosModules/pipewire.nix
-      ../../modules/nixosModules/bluetooth.nix
-      ../../modules/nixosModules/consoleKeymap.nix
-      ../../modules/nixosModules/vim.nix
-      ../../modules/nixosModules/wget.nix
-      ../../modules/nixosModules/fastfetch.nix
-      ../../modules/nixosModules/evtest.nix
-      ../../modules/nixosModules/modrinth-app.nix
-      ../../modules/nixosModules/alejandra.nix
-      ../../modules/nixosModules/keepassxc.nix
-      ../../modules/nixosModules/thunderbird.nix
-      ../../modules/nixosModules/ethtool.nix
-      ../../modules/nixosModules/libreoffice-still.nix
-      ../../modules/nixosModules/cpu-x.nix
-      ../../modules/nixosModules/prusa-slicer.nix
-      ../../modules/nixosModules/geekbench.nix
-      ../../modules/nixosModules/glmark2.nix
-      ../../modules/nixosModules/freecad-wayland.nix
-      ../../modules/nixosModules/openssh.nix
-      ../../modules/nixosModules/blueman.nix
-      ../../modules/nixosModules/networkmanager.nix
-      ../../modules/nixosModules/gdm.nix    # remove after hyprland
-      ../../modules/nixosModules/gnome.nix    # remove after hyprland
-      ../../modules/nixosModules/xkb.nix    # remove after hyprland
-      ../../modules/nixosModules/printing.nix
-      ../../modules/nixosModules/hardware.nix
-      ../../modules/nixosModules/pciutils.nix
-      ../../modules/nixosModules/btop-rocm.nix
-      ../../modules/nixosModules/Kernel_package_r8125.nix
-      ../../modules/nixosModules/neofetch.nix
-      ../../modules/nixosModules/trackpad.nix
-      ../../modules/nixosModules/fprintd.nix
+      ../../modules/nixosModules/defaults/gui_computer_defaults.nix
   ];
-
-#  services = {
-#    libinput.enable = true; # touchpad
-#  };
-
-#  fonts.packages = with pkgs; [
-#    noto-fonts
-#    noto-fonts-emoji
-#  ];
-
-#  services.xserver.enable = true;
 
   networking = {
     hostName = "nixtop";
-#    firewall.allowedTCPPorts = [22];
   };
 
   users.users.user = {
@@ -76,5 +22,5 @@
     extraGroups = ["wheel" "networkmanager"];
   };
 
-  system.stateVersion = "24.11"; # Don't change that do research !!!
+  system.stateVersion = "24.11";
 }
