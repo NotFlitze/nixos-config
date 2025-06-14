c0254bb9d57518e6fd99a44ac9894025d61fccde
NotFlitze <167577764+NotFlitze@users.noreply.github.com>
2025-06-15 00:29:26 +0200

    zfs and samba are working yay

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -1,4 +1,3 @@
-#                            
 #    ___ ___ _ ___ _____ _ _ 
 #   (_-</ -_) '_\ V / -_) '_|
 #   /__/\___|_|  \_/\___|_|  
@@ -18,16 +17,102 @@
 
     ];
 
+# --- module options --- #
+
   modules.bootloader.enable = false;
+
+
+
+# --- host spetific stuff --- #
 
   networking.hostName = "server";
 
   networking.hostId = "f6b91edc";
 
+  networking.firewall.enable = true;
+
+  networking.firewall.allowPing = true;
+
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "Server";
+        "security" = "user";
+        "map to guest" = "never";
+      };
+
+      "share" = {
+        "path" = "/nasdata/data/share";
+        "browseable" = "no";
+        "read only" = "no";
+        "valid users" = "sambauser1 sambauser2";
+        "force group" = "sambauser";
+        "create mask" = "0660";
+        "directory mask" = "0770";
+        "guest ok" = "no";
+      };
+
+      "sambauser1" = {
+        "path" = "/nasdata/data/sambauser1";
+        "browseable" = "no";
+        "read only" = "no";
+        "valid users" = "sambauser1";
+        "force group" = "sambauser";
+        "create mask" = "0600";
+        "directory mask" = "0700";
+        "guest ok" = "no";
+      };
+
+      "sambauser2" = {
+        "path" = "/nasdata/data/sambauser2";
+        "browseable" = "no";
+        "read only" = "no";
+        "valid users" = "sambauser2";
+        "force group" = "sambauser";
+        "create mask" = "0600";
+        "directory mask" = "0700";
+        "guest ok" = "no";
+      };
+    };
+  };
+
+  services.samba-wsdd = { 			# for Windows
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.avahi = { 				# for macOS/iOS & Linux-Clients per mDNS/Bonjour
+    publish.enable = true;
+    publish.userServices = true;
+    nssmdns4 = true;
+    enable = true;
+    openFirewall = true;
+  };
+
+# --- user + groups --- #
+
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
   };
+
+  users.users.sambauser1 = {
+    isNormalUser = true;
+    extraGroups = [ "sambauser" ];
+  };
+
+  users.users.sambauser2 = {
+    isNormalUser = true;
+    extraGroups = [ "sambauser" ];
+  };
+
+  users.groups.sambauser = {};
+
+# --- boot --- #
 
   boot = {
     supportedFilesystems = ["zfs"];
@@ -40,6 +125,8 @@
       zfsSupport = true;
     };
   };
+
+# --- #
 
   system.stateVersion = "25.05";
 
