d8d2943b3e16dd5bf583987a18e1296f15ce1a0c
NotFlitze <167577764+NotFlitze@users.noreply.github.com>
2025-06-28 22:50:33 +0200

    added backup zfs pool to backup server

--- hosts/backup-server/configuration.nix
+++ hosts/backup-server/configuration.nix
@@ -1,26 +1,25 @@
-#    _             _                                         
-#   | |__  __ _ __| |___  _ _ __ ___ ___ ___ _ ___ _____ _ _ 
+#    _             _
+#   | |__  __ _ __| |___  _ _ __ ___ ___ ___ _ ___ _____ _ _
 #   | '_ \/ _` / _| / / || | '_ \___(_-</ -_) '_\ V / -_) '_|
-#   |_.__/\__,_\__|_\_\\_,_| .__/   /__/\___|_|  \_/\___|_|  
-#                          |_|                               
+#   |_.__/\__,_\__|_\_\\_,_| .__/   /__/\___|_|  \_/\___|_|
+#                          |_|
 {
   config,
   pkgs,
   ...
 }: {
-  imports =
-    [
-      ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
 
-      ../../modules/nixosModules/defaults/cli_computer_defaults.nix
-    ];
+    ../../modules/nixosModules/defaults/cli_computer_defaults.nix
+  ];
 
   # --- module options --- #
 
   modules.bootloader.enable = false; # fix
-  
+
   # --- boot --- #
-  
+
   boot.loader.grub = {
     enable = true;
     efiSupport = false;
@@ -28,16 +27,23 @@
     device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_250GB_S3YJNB0K358813Y";
   };
 
+  boot = {
+    supportedFilesystems = ["zfs"];
+    zfs = {
+      forceImportRoot = false;
+#      extraPools = ["backup"];
+    };
+  };
+
   # --- host spetific stuff --- #
 
   networking.hostName = "backup-server";
 
-#  networking.hostId = "generate one";
+  networking.hostId = "feef2b19";
 
   networking.firewall.enable = true;
 
   networking.firewall.allowPing = true;
-
 
   # --- user + groups --- #
 
